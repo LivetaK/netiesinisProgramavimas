0236cf4bd28c2f4c8bfa8fdf930c0b91b360dbfd
Liveta <livetakaval@gmail.com>
2025-04-25 22:02:04 +0300

    -

--- netiesinisProgramavimas/functions.py
+++ netiesinisProgramavimas/functions.py
@@ -1,34 +1,27 @@
 import math
 
 def tikslo_funkcija(taskas):
-    x1 = taskas[0]
-    x2 = taskas[1]
-    x3 = taskas[2]
-    f = (1.0 / 8) * (x1 * x2 * x3)
+    a = taskas[0]
+    b = taskas[1]
+    c = taskas[2]
+    f = a*b*c 
     return -f
 
 def gi(taskas):
-    x1 = taskas[0]
-    x2 = taskas[1]
-    x3 = taskas[2]
-    return x1+x2+x3 - 1
+    a = taskas[0]
+    b = taskas[1]
+    c = taskas[2]
+    return 2*(a*b+b*c+a*c) - 1
 
 def hi(taskas):
-    x1 = -taskas[0]
-    x2 = -taskas[1]
-    x3 = -taskas[2]
-    naujas_taskas = [x1, x2, x3]
-    return naujas_taskas
+    return [max(0, -x) for x in taskas]
     
 def bauda(taskas, r):
-    h = hi(taskas)
     g = gi(taskas)
-    b = 0
-    for t in h:
-        if t > 0:
-            b += t*t
-    b+= g * g
-    return tikslo_funkcija(taskas) + 1.1/r * b
+    b = sum((max(0, -x))**2 for x in taskas)
+    b += g**2
+    return tikslo_funkcija(taskas) + r * b
+
 
 
 def test():
@@ -43,41 +36,41 @@ def test():
     return input("Jei nebenorite testi, iveskite 'n', jei norite testi, iveskite 't'\n").lower()
 
 
+def antigradientas(taskas, r, h=1e-5):
+    base = bauda(taskas, r)
+    grad = []
+    for i in range(len(taskas)):
+        t = taskas[:]
+        t[i] += h
+        delta = (bauda(t, r) - base) / h
+        grad.append(-delta)
+    return grad
 
 
-def antigradientas(taskas):
-    x1 = taskas[0]
-    x2 = taskas[1]
-    x3 = taskas[2]
-    fGrad = [
-	-1.0 / 8 * x2 * x3,
-    -1.0 / 8 * x1 * x3,
-    -1.0 / 8 * x1 * x2]
-    return fGrad;
 
-
-def auksoPjuvis(taskas):
+def auksoPjuvis(taskas, r):
     auksoPjuv = (math.sqrt(5) - 1) / 2
     a = 0.0
-    b = 1.0
+    b = 10.0
     epsilon = 1e-6
     
     x1 = b - auksoPjuv * (b - a)
     x2 = a + auksoPjuv * (b - a)
+    grad = antigradientas(taskas, r)
     
     taskasX1 = [
-        taskas[0] - x1 * antigradientas(taskas)[0],
-        taskas[1] - x1 * antigradientas(taskas)[1],
-        taskas[2] - x1 * antigradientas(taskas)[2]
+        taskas[0] - x1 * grad[0],
+        taskas[1] - x1 * grad[1],
+        taskas[2] - x1 * grad[2]
     ]
     taskasX2 = [
-		taskas[0] - x2 * antigradientas(taskas)[0],
-		taskas[1] - x2 * antigradientas(taskas)[1],
-		taskas[2] - x2 * antigradientas(taskas)[2]
+		taskas[0] - x2 * grad[0],
+		taskas[1] - x2 * grad[1],
+		taskas[2] - x2 * grad[2]
 	]
 
-    fx1 = tikslo_funkcija(taskasX1); 
-    fx2 = tikslo_funkcija(taskasX2)
+    fx1 = bauda(taskasX1, r)
+    fx2 = bauda(taskasX2, r)
     
     while abs(b - a) > epsilon:
         if fx1 < fx2:
@@ -87,11 +80,11 @@ def auksoPjuvis(taskas):
             x1 = b - auksoPjuv * (b - a)
             
             taskasX1 = [
-                taskas[0] - x1 * antigradientas(taskas)[0],
-                taskas[1] - x1 * antigradientas(taskas)[1],
-                taskas[2] - x1 * antigradientas(taskas)[2]
+                taskas[0] - x1 * grad[0],
+                taskas[1] - x1 * grad[1],
+                taskas[2] - x1 * grad[2]
             ]
-            fx1 = tikslo_funkcija(taskasX1)
+            fx1 = bauda(taskasX1, r)
         else:
             a = x1
             x1 = x2
@@ -99,25 +92,40 @@ def auksoPjuvis(taskas):
             x2 = a + auksoPjuv * (b - a)
             
             taskasX2 = [
-                taskas[0] - x2 * antigradientas(taskas)[0],
-                taskas[1] - x2 * antigradientas(taskas)[1],
-                taskas[2] - x2 * antigradientas(taskas)[2]
+                taskas[0] - x2 * grad[0],
+                taskas[1] - x2 * grad[1],
+                taskas[2] - x2 * grad[2]
             ]
-            fx2 = tikslo_funkcija(taskasX2)
+            fx2 = bauda(taskasX2, r)
+
     
     return (a + b) / 2
 
 def greiciausiasNusileidimas(taskas):
-    grad = antigradientas(taskas)
+    r = 10.0
+    grad = antigradientas(taskas, r)
     epsilon = 1e-6
-    while abs(grad[0]) > epsilon or abs(grad[1]) > epsilon:
-        gamma = auksoPjuvis(taskas)
+    for i in range(1000):
+        norma = math.sqrt(sum(g**2 for g in grad))
+        if norma < epsilon:
+            break
+
+        print("****************************************************************")
+        print(f"\nITERACIJA {i+1}")
+        print(f"Taskas: {taskas}")
+        print(f"Baudos funkcijos reiksme: {bauda(taskas, r)}")
+        print(f"Gradiento norma: {norma}")
+        print(f"Tiklso funkcijos reiksme: {tikslo_funkcija(taskas)}")
+        r *= 1.2
+        gamma = auksoPjuvis(taskas, r)
         naujasTaskas = [
-        taskas[0] - gamma * grad[0],
-        taskas[1] - gamma * grad[1],
-        taskas[2] - gamma * grad[2]
+            taskas[0] - gamma * grad[0],
+            taskas[1] - gamma * grad[1],
+            taskas[2] - gamma * grad[2]
         ]
-        grad = antigradientas(naujasTaskas)
+        grad = antigradientas(naujasTaskas, r)
+        taskas = naujasTaskas
+        print(taskas)
 
     print(f"Minimumo taskas: {taskas[0]} {taskas[1]} {taskas[2]}")
     print(f"Minimumo reiksme: {tikslo_funkcija(taskas)}")
--- netiesinisProgramavimas/netiesinisProgramavimas.py
+++ netiesinisProgramavimas/netiesinisProgramavimas.py
@@ -2,11 +2,11 @@ from functions import *
 
 
 #testavimas
-pasirinkimas = 't'
-while pasirinkimas != 'n':
-    pasirinkimas = test()
-    while pasirinkimas != 'n' and pasirinkimas !='t':
-        print("Neteisingas pasirinkimas")
-        pasirinkimas = input("Jei nebenorite testi, iveskite 'n', jei norite testi, iveskite 't'\n").lower()
-
-greiciausiasNusileidimas(1.0, 1.0, 1.0)
+# pasirinkimas = 't'
+# while pasirinkimas != 'n':
+#     pasirinkimas = test()
+#     while pasirinkimas != 'n' and pasirinkimas !='t':
+#         print("Neteisingas pasirinkimas")
+#         pasirinkimas = input("Jei nebenorite testi, iveskite 'n', jei norite testi, iveskite 't'\n").lower()
+taskas = [0.6, 0.3, 0.4]
+greiciausiasNusileidimas(taskas)
